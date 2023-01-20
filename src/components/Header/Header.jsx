42dd81a9af52ae8826b53974d1ca3222825db31a
Serghei Granici <granici.design@gmail.com>
2023-01-20 17:43:26 +0100

    Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.module.scss';
+
+function Header() {
+  return (
+    <div className="Header">
+     <div className="Logo">
+      <img src="./img/Logo.svg" alt="Logo" />
+     </div>
+     <div></div>
+     <div></div>
+     <div></div>
+    </div>
+  );
+}
+
+export default Header;
